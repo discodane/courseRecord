9772884cc27bab8310ddb1829830f43ab2c5e8e8
Dane Erickson <el.dane.erickson@gmail.com>
2026-04-27 15:00:11 -0600

    Initial Commit

---
+++ courseRecord/courseRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  courseRecordApp.swift
+//  courseRecord
+//
+//  Created by Dane Erickson on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct courseRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ courseRecordTests/courseRecordTests.swift
@@ -0,0 +1,17 @@
+//
+//  courseRecordTests.swift
+//  courseRecordTests
+//
+//  Created by Dane Erickson on 4/27/26.
+//
+
+import Testing
+@testable import courseRecord
+
+struct courseRecordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
